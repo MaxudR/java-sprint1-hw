41d4f84852f71776bb4abc9d945b2e68128bce68
Maxud <Maximodik95@yandex.ru>
2022-06-28 16:42:22 +0300

    Сделано командное меню, добавлены классы StepTracker и Converter

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Пришло время практики!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            printMenu();
+            int command = scanner.nextInt();
+
+            if (command == 1) {
+                System.out.println("За какой месяц текущего года вы хотите ввести количество шагов? Введите число, от 0 до 11, соответствующее месяцу года");
+                int month = scanner.nextInt();
+                System.out.println("За какой день вы хотите ввести количество шагов? Введите число, от 1 до 30, соответствующее дню месяца");
+                int day = scanner.nextInt();
+                System.out.println("Введите количество шагов");
+                double countStep = scanner.nextDouble();
+            } else if (command == 2) {
+
+            } else if (command == 3) {
+
+            } else if (command == 4) {
+                System.out.println("Выход из приложения");
+                break;
+            } else {
+                System.out.println("Извините, такой операции нет в списке.");
+            }
+        }
+    }
+
+
+    public static void printMenu() {
+        System.out.println("Введите цифру для выполнения соответствующей операции");
+        System.out.println("1 - Ввести количество шагов за определённый день");
+        System.out.println("2 - Напечатать статистику за определённый месяц");
+        System.out.println("3 - Изменить цель по количеству шагов в день");
+        System.out.println("4 - Выйти из приложения");
     }
 }
+
+
